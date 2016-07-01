2afa67453d22647f280124473c920b37a8c4d688
Nelson Dominguez <ndleon09@gmail.com>
2016-07-02 01:38:06 +0200

    Remove BuddyBuildSDK

--- TopApps/AppDelegate.swift
+++ TopApps/AppDelegate.swift
@@ -15,9 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool
-    {
-        BuddyBuildSDK.setup()
-        
+    {        
         window = UIWindow(frame:UIScreen.mainScreen().bounds)
         window?.rootViewController = ServiceLocator().rootViewController()
         
