948c810f8e234aed9764fe28f1b6555928196066
buddybuild <help@buddybuild.com>
2016-07-01 15:33:16 -0700

    Adding buddybuild SDK
    - Add BuddyBuildSDK.framework
    - Initialize in App Delegate

--- TopApps/AppDelegate.swift
+++ TopApps/AppDelegate.swift
@@ -16,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool
     {
+        BuddyBuildSDK.setup()
+        
         window = UIWindow(frame:UIScreen.mainScreen().bounds)
         window?.rootViewController = ServiceLocator().rootViewController()
         
